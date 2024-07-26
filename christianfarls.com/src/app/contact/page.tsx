e610a084f385fb6c7b4bea17c4d32d4776b779ae
christianfarls <cfarls@nd.edu>
2024-07-26 00:36:49 -0400

    updated contact page and started contact form

--- christianfarls.com/src/app/contact/page.tsx
+++ christianfarls.com/src/app/contact/page.tsx
@@ -1,57 +1,23 @@
-import PROJECTS from '@/data/projects'
-import { AspectRatio } from '@/components/ui/aspect-ratio'
+"use client";
 
-export default function Work() {
-  return (
-    <div>
-      <h1 className="mb-8 text-2xl font-heading sm:text-4xl">Contact</h1>
+import {SubmitHandler} from "react-hook-form";
 
-      <div className="flex flex-col gap-5">
-        {PROJECTS.map((project, id) => {
-          return (
-            <div
-              className="border-border dark:border-darkBorder shadow-light dark:shadow-dark rounded-base border-2 bg-main p-4 sm:p-5"
-              key={id}
-            >
-              <AspectRatio
-                className="border-border dark:border-darkBorder shadow-light dark:shadow-dark !-bottom-[2px] rounded-base border-2"
-                ratio={71 / 26}
-              >
-                <img
-                  className="w-full rounded-base"
-                  src={`${project.previewImage}`}
-                  alt={project.name}
-                />
-              </AspectRatio>
+// Main Contact component
+export default function Contact() {
+    const handleSubmit: SubmitHandler<FormData> = (data) => {
+        // Handle form submission
+        console.log(data);
+    };
 
-              <div className="text-text mt-5 font-base">
-                <h2 className="text-xl font-heading sm:text-2xl">
-                  {project.name}
-                </h2>
-
-                <p className="mt-2">{project.description}</p>
-
-                <div className="mt-8 grid grid-cols-2 gap-5">
-                  <a
-                    className="border-border dark:border-darkBorder shadow-light dark:shadow-dark cursor-pointer rounded-base border-2 bg-white px-4 py-2 text-center text-sm font-base transition-all hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none sm:text-base dark:hover:shadow-none"
-                    href={project.liveLink}
-                    target="_blank"
-                  >
-                    Visit
-                  </a>
-                  <a
-                    className="border-border dark:border-darkBorder shadow-light dark:shadow-dark cursor-pointer rounded-base border-2 bg-white px-4 py-2 text-center text-sm font-base transition-all hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none sm:text-base dark:hover:shadow-none"
-                    href={project.repoUrl}
-                    target="_blank"
-                  >
-                    Github
-                  </a>
-                </div>
-              </div>
+    return (
+        <div className="">
+            <h1 className="mb-8 text-2xl font-heading sm:text-4xl">Contact</h1>
+            <p>You can reach me at +1 (412) 713-5531 or cfarls@nd.edu. Hope to chat soon!</p>
+            {/*
+            <div className="flex flex-col gap-5 items-center">
+                <ContactForm onSubmit={handleSubmit}/>
             </div>
-          )
-        })}
-      </div>
-    </div>
-  )
+            */}
+        </div>
+    );
 }
