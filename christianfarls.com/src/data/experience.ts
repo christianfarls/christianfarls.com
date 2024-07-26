c0b9d7180c50c646d34c55ab2936f189b19d40f5
christianfarls <cfarls@nd.edu>
2024-07-26 00:00:31 -0400

    added experiences

--- christianfarls.com/src/data/experience.ts
+++ christianfarls.com/src/data/experience.ts
@@ -1,34 +1,33 @@
 const PAST_ROLES: {
-  company: string
-  role: string
-  description: string
-  startDate: string
-  endDate: string
+    company: string
+    role: string
+    description: string
+    startDate: string
+    endDate: string
 }[] = [
-  {
-    company: 'ACME Solutions',
-    role: 'CEO',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel hic aspernatur molestiae exercitationem rerum reiciendis mollitia magnam harum ipsum?',
-    startDate: 'March 2023',
-    endDate: 'Now',
-  },
-  {
-    company: 'Google',
-    role: 'Software Engineer',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel hic aspernatur molestiae exercitationem rerum reiciendis mollitia magnam harum ipsum?',
-    startDate: 'April 2021',
-    endDate: 'March 2023',
-  },
-  {
-    company: 'Vercel',
-    role: 'Developer Advocate',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel hic aspernatur molestiae exercitationem rerum reiciendis mollitia magnam harum ipsum?',
-    startDate: 'April 2021',
-    endDate: 'June 2018',
-  },
+    {
+        company: 'Mitsubishi Electric Power Products',
+        role: 'Software Engineer Intern',
+        description:
+            'Redesigned key page layouts using React, enhancing user experience and consistency. Converted a data job ' +
+            'into on-demand services, improving responsiveness and data management. Integrated computer vision models, ' +
+            'contributing to advanced visual features. Gained experience in full stack development, including front-end, ' +
+            'back-end, database management, and machine learning integration.',
+        startDate: 'May 2023',
+        endDate: 'August 2024',
+    },
+    {
+        company: 'Notre Dame Center for Research Computing',
+        role: 'Summer Research and Development Intern',
+        description:
+            'Conducted in-depth research on various game development libraries to evaluate their compatibility and ' +
+            'performance for an upcoming project built in React, including testing several components to assess their ' +
+            'functionality and integration. Collaborated with a scrum team, gaining hands-on experience in agile ' +
+            'methodologies, including daily stand-ups, sprint planning, and retrospectives. ' +
+            'Enhanced understanding of the full game development lifecycle and agile project management principles.',
+        startDate: 'April 2021',
+        endDate: 'June 2018',
+    },
 ]
 
 export default PAST_ROLES
