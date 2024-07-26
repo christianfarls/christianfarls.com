fdbe10e434f9574bd2a8f4eef7b098b26862c337
christianfarls <cfarls@nd.edu>
2024-07-25 23:03:53 -0400

    updated links on home

--- christianfarls.com/src/components/links.tsx
+++ christianfarls.com/src/components/links.tsx
@@ -1,40 +1,34 @@
-import {
-  IconType,
-  SiGithub,
-  SiGmail,
-  SiLinkedin,
-  SiMedium,
-} from '@icons-pack/react-simple-icons'
+import {IconType, SiGithub, SiGmail, SiLinkedin, SiX,} from '@icons-pack/react-simple-icons'
 
 export default function Links() {
-  const links: { icon: IconType; href: string }[] = [
-    {
-      icon: SiGmail,
-      href: 'mailto:johndoe@gmail.com',
-    },
-    {
-      icon: SiGithub,
-      href: 'https://github.com/johndoe',
-    },
-    {
-      icon: SiLinkedin,
-      href: 'https://www.linkedin.com/in/johndoe/',
-    },
-    {
-      icon: SiMedium,
-      href: 'https://medium.com/@johndoe',
-    },
-  ]
+    const links: { icon: IconType; href: string }[] = [
+        {
+            icon: SiGmail,
+            href: 'mailto:cfarls@nd.edu',
+        },
+        {
+            icon: SiGithub,
+            href: 'https://github.com/christianfarls',
+        },
+        {
+            icon: SiLinkedin,
+            href: 'https://www.linkedin.com/in/christianfarls/',
+        },
+        {
+            icon: SiX,
+            href: 'https://x.com/@christianfarls',
+        },
+    ]
 
-  return (
-    <div className="mr-auto mt-20 flex w-full flex-wrap items-center gap-10">
-      {links.map((link, id) => {
-        return (
-          <a target="_blank" key={id} href={link.href}>
-            <link.icon title="" />
-          </a>
-        )
-      })}
-    </div>
-  )
+    return (
+        <div className="mr-auto mt-20 flex w-full flex-wrap items-center gap-10">
+            {links.map((link, id) => {
+                return (
+                    <a target="_blank" key={id} href={link.href}>
+                        <link.icon title=""/>
+                    </a>
+                )
+            })}
+        </div>
+    )
 }
