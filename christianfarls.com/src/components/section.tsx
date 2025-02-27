992de8b643e2ff45efb97e3eb90c27b5206045cb
Christian Farls <cfarls@nd.edu>
2025-02-27 03:14:49 -0500

    fix: section prop passing

--- christianfarls.com/src/components/section.tsx
+++ christianfarls.com/src/components/section.tsx
@@ -1,5 +1,11 @@
-'use client'
+"use client";
 
-export default function Section({ children }) {
+import { ReactNode } from "react";
+
+interface SectionProps {
+    children: ReactNode;
+}
+
+export default function Section({ children }: SectionProps) {
     return <section className="mb-4">{children}</section>;
 }
